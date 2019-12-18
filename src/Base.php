fc7daf90872ab9bc0b1a4e3d471a53d98309e5da
忆坛酒厂 <ahegege@126.com>
2019-12-18 14:49:57 +0800

    。。

--- src/Base.php
+++ src/Base.php
@@ -1,9 +1,27 @@
 <?php
+
 namespace thinkyaf;
 
-abstract class Base{
-    public static function test()
+use \Yaf\Application;
+use Yaf\Dispatcher;
+use Yaf\Loader;
+
+define("Think_PATH",  realpath(dirname(__FILE__) . '/'));
+class Base
+{
+    public function __construct()
     {
-        echo 'thinkyaf:base';
+        $this->app = new Application(Think_PATH . "/thinkyaf.ini");
+        $this->config = Application::app()->getConfig();
+        // 设置常量
+        define("APP_PATH",  $this->config->application->directory);
+        // 加载公共文件
+        Loader::import(APP_PATH . '/common.php');
+        Dispatcher::getInstance()->autoRender(FALSE);
+    }
+    // 初始化
+    function init()
+    {
+        return $this;
     }
 }
